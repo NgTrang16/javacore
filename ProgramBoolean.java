a6482cc42f537fe3ff1490b0fcbe515459e77b68
Ngân Trang <ngantrang1603@gmail.com>
2025-07-11 00:24:53 +0700

    Create prj

---
+++ ProgramBoolean.java
@@ -0,0 +1,10 @@
+public class ProgramBoolean {
+    public static void main(String[] args) {
+        boolean isMan = true;
+        System.out.println("Is man: " +isMan);
+        boolean isRich = false;
+        System.out.println("Is rich: " +isRich);
+        String fullName = "ngoc";
+        System.out.println(fullName);
+    }
+}
---
+++ bai5_1.java
@@ -0,0 +1,15 @@
+public class bai5_1 {
+    public static void main(String[] args) {
+        int age = 33;
+        long money = 1000;
+        System.out.println("tuoi: " + age);
+        System.out.println("money:  " + money);
+        short age2 = 128;
+        System.out.println("age2: " + age2);
+        byte age3 = 127;
+        System.out.println("tuoi3: " + age3);
+
+        float score = 9.5F;
+        System.out.println("diem: " + score);
+    }
+}
